ab1770af343ec98b8dce761b2c60ae47f713997d
Vaporox <jan.zichler@gmx.de>
2022-01-14 16:57:29 +0100

    refactor: order api methods alphabetically

--- examples/main.rs
+++ examples/main.rs
@@ -9,12 +9,12 @@ async fn main() {
 		Err(error) => println!("Error: {}", error),
 	}
 
-	match client.user(71).await {
+	match client.search_user("Vaporox").await {
 		Ok(user) => println!("Found user: {} (Account ID: {})", user.username, user.account_id),
 		Err(error) => println!("Error: {}", error),
 	}
 
-	match client.search_user("Vaporox").await {
+	match client.user(71).await {
 		Ok(user) => println!("Found user: {} (Account ID: {})", user.username, user.account_id),
 		Err(error) => println!("Error: {}", error),
 	}
