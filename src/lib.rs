55c7b61a80ae9e3cc4a039c98b52d038d96aa936
Vaporox <jan.zichler@gmx.de>
2022-04-17 16:38:30 +0200

    chore: set custom logo for docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! A wrapper around the Geometry Dash API.
 
+#![doc(html_logo_url = "https://raw.githubusercontent.com/gdapi-rs/.github/main/assets/icon.png")]
+
 mod constants;
 mod form;
 mod http;
