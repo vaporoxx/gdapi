47ae0ac68d48d7611c2195bd502a9fab8ce7b115
Vaporox <jan.zichler@gmx.de>
2022-04-21 23:30:18 +0200

    chore: add favicon url

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 //! A wrapper around the Geometry Dash API.
 
-#![doc(html_logo_url = "https://raw.githubusercontent.com/gdapi-rs/.github/main/assets/icon.png")]
+#![doc(
+	html_favicon_url = "https://raw.githubusercontent.com/gdapi-rs/.github/main/assets/icon.png",
+	html_logo_url = "https://raw.githubusercontent.com/gdapi-rs/.github/main/assets/icon.png"
+)]
 
 mod constants;
 mod form;
